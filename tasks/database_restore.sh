089e368bc88669af38a61061ae0ea7c7ea34d2d3
Romain Tartière <romain@blogreen.org>
2020-07-22 08:38:01 -1000

    Add tasks for database backup & restore

---
+++ tasks/database_restore.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -f "${PT_filename}" ]; then
+	echo "The backup ${PT_filename} MUST exist" >&2
+	exit 1
+fi
+
+if [ -d ~odoo/.local/share/Odoo/filestore/"${PT_name}" ]; then
+	echo "The database ${PT_name} MUST NOT exist" >&2
+	exit 1
+fi
+
+TMPDIR=`mktemp -d`
+
+(
+	cd "$TMPDIR"
+	unzip "${PT_filename}"
+)
+chown -R odoo:odoo "${TMPDIR}/filestore"
+mv "${TMPDIR}/filestore" ~odoo/.local/share/Odoo/filestore/"${PT_name}"
+createdb -Oodoo -Eunicode "${PT_name}"
+sudo -u postgres psql "${PT_name}" < "${TMPDIR}/dump.sql"
+
+rm -r "${TMPDIR}"
