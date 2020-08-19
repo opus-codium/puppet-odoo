4ee5eccadc592c65d44d200e75c23541c9aa0d74
Romain Tartière <romain@blogreen.org>
2020-08-18 22:21:45 -1000

    Make hash elements order deterministic
    Sort matched key/values in an ordered dict to make the items order
    deterministic.

--- files/databases-list.py
+++ files/databases-list.py
@@ -1,12 +1,13 @@
 import json
+from collections import OrderedDict
 
 res = {}
 
 for dbname in odoo.service.db.list_dbs(True):
-    res[dbname] = {}
+    res[dbname] = OrderedDict()
     registry = odoo.registry(dbname)
     with registry.cursor() as cr:
-        cr.execute("SELECT key, value FROM ir_config_parameter WHERE key IN ('database.create_date', 'database.enterprise_code', 'database.expiration_date', 'database.expiration_reason', 'web.base.url')")
+        cr.execute("SELECT key, value FROM ir_config_parameter WHERE key IN ('database.create_date', 'database.enterprise_code', 'database.expiration_date', 'database.expiration_reason', 'web.base.url') ORDER BY key")
         for line in cr.fetchall():
             res[dbname][line[0]] = line[1]
 
