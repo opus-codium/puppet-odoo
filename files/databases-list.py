8109bfef5552c3a82c56d404859311696bf22071
Romain Tartière <romain@blogreen.org>
2020-12-30 07:35:19 -1000

    Report database.uuid in odoo::databases_list task
    The Odoo company seems to not be able to track/handle these correctly
    and we must remind them what these UUIDs are from time to time.

--- files/databases-list.py
+++ files/databases-list.py
@@ -7,7 +7,7 @@ for dbname in odoo.service.db.list_dbs(True):
     res[dbname] = OrderedDict()
     registry = odoo.registry(dbname)
     with registry.cursor() as cr:
-        cr.execute("SELECT key, value FROM ir_config_parameter WHERE key IN ('database.create_date', 'database.enterprise_code', 'database.expiration_date', 'database.expiration_reason', 'web.base.url') ORDER BY key")
+        cr.execute("SELECT key, value FROM ir_config_parameter WHERE key IN ('database.create_date', 'database.enterprise_code', 'database.expiration_date', 'database.expiration_reason', 'web.base.url', 'database.uuid') ORDER BY key")
         for line in cr.fetchall():
             res[dbname][line[0]] = line[1]
 
