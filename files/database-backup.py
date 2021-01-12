fc7f6e8ac43daeb714656df7da0608da598e3bd2
Romain Tartière <romain@blogreen.org>
2021-01-12 08:48:23 -1000

    Open file for writing
    Dumping a database need a file descriptor on a file opened for writing…
    Default is reading.
    
    Fix that!

--- files/database-backup.py
+++ files/database-backup.py
@@ -2,5 +2,5 @@ import os
 
 odoo.tools.config['list_db'] = True
 
-with open(os.environ['PT_filename']) as t:
+with open(os.environ['PT_filename'], mode='w+b') as t:
     odoo.service.db.dump_db(os.environ['PT_name'], t, os.environ['PT_format'])
