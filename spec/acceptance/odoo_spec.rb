108d558a9d18c3cba77241baff767d0d77c241f5
Romain Tartière <romain@blogreen.org>
2021-02-17 10:38:23 -1000

    Detect incompatible Odoo and OS versions

--- spec/acceptance/odoo_spec.rb
+++ spec/acceptance/odoo_spec.rb
@@ -1,42 +1,58 @@
 require 'spec_helper_acceptance'
 
+def odoo_supported_versions
+  {
+    'Debian' => {
+      '9'  => ['11.0', '12.0', '13.0'],
+      '10' => ['11.0', '12.0', '13.0'],
+    },
+    'Ubuntu' => {
+      '16.04' => ['10.0', '11.0', '12.0', '13.0'],
+      '18.04' => ['11.0', '12.0', '13.0'],
+      '20.04' => ['11.0', '12.0', '13.0'],
+    },
+  }[fact('os.name')][fact('os.release.major')]
+end
+
 describe 'odoo class' do
   ['10.0', '11.0', '12.0', '13.0'].each do |version|
     context "when using odoo #{version}" do
       let(:version) { version }
 
-      it 'works idempotently with no errors' do
-        # FIXME: We should not tweak anything here, the odoo class should be self-contained
-        pp = <<~MANIFEST
-      if $facts.get('os.name') == 'debian' {
-        class { 'apt::backports':
-        }
-      }
-      if $facts.get('os.name') == 'ubuntu' {
-        apt::source { 'ubuntu-universe':
-          location => 'http://archive.ubuntu.com/ubuntu',
-          repos    => 'universe',
-        }
-      }
+      if odoo_supported_versions.include?(version)
+        it 'works idempotently with no errors' do
+          # FIXME: We should not tweak anything here, the odoo class should be self-contained
+          pp = <<~MANIFEST
+            if $facts.get('os.name') == 'debian' {
+              class { 'apt::backports':
+              }
+            }
+            if $facts.get('os.name') == 'ubuntu' {
+              apt::source { 'ubuntu-universe':
+                location => 'http://archive.ubuntu.com/ubuntu',
+                repos    => 'universe',
+              }
+            }
 
-      package { 'python-pip':
-        ensure => installed,
-      }
+            package { 'python-pip':
+              ensure => installed,
+            }
 
-      package { 'python3-pip':
-        ensure => installed,
-      }
+            package { 'python3-pip':
+              ensure => installed,
+            }
 
-      class { 'odoo':
-        version => '#{version}'
-      }
+            class { 'odoo':
+              version => '#{version}'
+            }
 
-      Class['apt::update']
-      -> Class['odoo::dependencies']
-        MANIFEST
+            Class['apt::update']
+            -> Class['odoo::dependencies']
+          MANIFEST
 
-        apply_manifest(pp, catch_failures: true)
-        apply_manifest(pp, catch_changes: true)
+          apply_manifest(pp, catch_failures: true)
+          apply_manifest(pp, catch_changes: true)
+        end
       end
     end
   end
