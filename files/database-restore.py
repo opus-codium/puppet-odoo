b2779b3e493647459a0426cd0f50dd721173a516
Romain Tartière <romain@blogreen.org>
2021-05-12 16:36:00 -1000

    Fix odoo::database_restore
    The odoo.service.db.exp_restore() function expects a base64 encoded
    stream of the file to restore, not the filename of the file to restore.

--- files/database-restore.py
+++ files/database-restore.py
@@ -1,4 +1,5 @@
+import base64
 import os
 
 odoo.tools.config['list_db'] = True
-odoo.service.db.exp_restore(os.environ['PT_name'], os.environ['PT_filename'], os.environ['PT_copy'].lower() in ('yes', 'true', 't', 'y', '1'))
+odoo.service.db.exp_restore(os.environ['PT_name'], base64.b64encode(open(os.environ['PT_filename'], 'rb').read()), os.environ['PT_copy'].lower() in ('yes', 'true', 't', 'y', '1'))
