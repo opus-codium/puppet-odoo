54f203b9a570704fd3591cb28a583a3724d8f8c2
Romain Tartière <romain@blogreen.org>
2021-08-19 08:59:28 -1000

    Simplify / Fix test matrix

--- spec/acceptance/odoo_spec.rb
+++ spec/acceptance/odoo_spec.rb
@@ -14,46 +14,44 @@ def odoo_supported_versions
 end
 
 describe 'odoo class' do
-  ['10.0', '11.0', '12.0', '13.0'].each do |version|
+  odoo_supported_versions.each do |version|
     context "when using odoo #{version}" do
       let(:version) { version }
 
-      if odoo_supported_versions.include?(version)
-        it 'works idempotently with no errors' do
-          # FIXME: We should not tweak anything here, the odoo class should be self-contained
-          pp = <<~MANIFEST
-            if $facts.get('os.name') == 'debian' {
-              class { 'apt::backports':
-              }
+      it 'works idempotently with no errors' do
+        # FIXME: We should not tweak anything here, the odoo class should be self-contained
+        pp = <<~MANIFEST
+          if $facts.get('os.name') == 'debian' {
+            class { 'apt::backports':
             }
-            if $facts.get('os.name') == 'ubuntu' {
-              apt::source { 'ubuntu-universe':
-                location => 'http://archive.ubuntu.com/ubuntu',
-                repos    => 'universe',
-              }
+          }
+          if $facts.get('os.name') == 'ubuntu' {
+            apt::source { 'ubuntu-universe':
+              location => 'http://archive.ubuntu.com/ubuntu',
+              repos    => 'universe',
             }
+          }
 
-            if '#{version}' == '10.0' {
-              package { 'python-pip':
-                ensure => installed,
-              }
-            } else {
-              package { 'python3-pip':
-                ensure => installed,
-              }
+          if '#{version}' == '10.0' {
+            package { 'python-pip':
+              ensure => installed,
             }
-
-            class { 'odoo':
-              version => '#{version}'
+          } else {
+            package { 'python3-pip':
+              ensure => installed,
             }
+          }
 
-            Class['apt::update']
-            -> Class['odoo::dependencies']
-          MANIFEST
+          class { 'odoo':
+            version => '#{version}'
+          }
 
-          apply_manifest(pp, catch_failures: true)
-          apply_manifest(pp, catch_changes: true)
-        end
+          Class['apt::update']
+          -> Class['odoo::dependencies']
+        MANIFEST
+
+        apply_manifest(pp, catch_failures: true)
+        apply_manifest(pp, catch_changes: true)
       end
     end
   end
