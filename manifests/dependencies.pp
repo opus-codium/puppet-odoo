12f6219fa5ae4af26100932b7a2e5ce173bf1dae
Romain Tartière <romain@blogreen.org>
2024-06-05 15:12:38 -1000

    Add support for Ubuntu 24.04
    While here, remove compatibility bits for odoo versions older than 13.0
    and complain harder when extra dependencies are not explicitly listed.

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -11,68 +11,63 @@ class odoo::dependencies {
       ensure => installed,
     }
 
-    if versioncmp($odoo::version, '13.0') < 0 {
-      # These dependencies are only listed on the Odoo 11 and Odoo 12 setup pages:
-      # https://www.odoo.com/documentation/11.0/setup/install.html
-      # https://www.odoo.com/documentation/12.0/setup/install.html
-
-      case $facts.get('os.name') {
-        'debian': {
-          case $facts.get('os.release.major') {
-            '11': {
-              $odoo_dependencies = [
-                'python3-qrcode',
-                'python3-phonenumbers',
-                'python3-pyldap',
-                'python3-vobject',
-              ]
-              $odoo_pip_packages = {
-                'num2words' => { ensure => '0.5.10' },
-              }
-            }
-            '12': {
-              $odoo_dependencies = [
-                'python3-qrcode',
-                'python3-phonenumbers',
-                'python3-pyldap',
-                'python3-vobject',
-                'python3-num2words',
-              ]
-              $odoo_pip_packages = {}
-            }
-            default: {
-              warning("Please contribute support for Debian ${facts.get('os.release.full')}")
-              $odoo_dependencies = []
-              $odoo_pip_packages = {}
-            }
+    case $facts.get('os.name') {
+      'debian': {
+        case $facts.get('os.release.major') {
+          '11': {
+            $odoo_dependencies = []
+            $odoo_pip_packages = {}
+          }
+          '12': {
+            $odoo_dependencies = []
+            $odoo_pip_packages = {}
+          }
+          default: {
+            notice { "Please contribute support for Debian ${facts.get('os.release.full')}": }
+            $odoo_dependencies = []
+            $odoo_pip_packages = {}
           }
         }
-        'ubuntu': {
-          case $facts.get('os.release.full') {
-            default: {
-              warning("Please contribute support for ubuntu ${facts.get('os.release.full')}")
-              $odoo_dependencies = []
-              $odoo_pip_packages = {}
-            }
+      }
+      'ubuntu': {
+        case $facts.get('os.release.full') {
+          '20.04': {
+            $odoo_dependencies = []
+            $odoo_pip_packages = {}
+          }
+          '22.04': {
+            $odoo_dependencies = []
+            $odoo_pip_packages = {}
+          }
+          '24.04': {
+            $odoo_dependencies = [
+              'python3-lxml-html-clean',
+            ]
+            $odoo_pip_packages = {}
+          }
+          default: {
+            notice { "Please contribute support for ubuntu ${facts.get('os.release.full')}": }
+            $odoo_dependencies = []
+            $odoo_pip_packages = {}
           }
         }
-        default: {
-          warning("Please contribute support for ${facts.get('os.name')}")
-          $odoo_dependencies = []
-          $odoo_pip_packages = {}
-        }
       }
-
-      package { $odoo_dependencies:
-        ensure => installed,
+      default: {
+        notice { "Please contribute support for ${facts.get('os.name')}": }
+        $odoo_dependencies = []
+        $odoo_pip_packages = {}
       }
+    }
 
-      $odoo_pip_packages.each |$name, $options| {
-        package { $name:
-          provider => $odoo::pip_provider,
-          require  => Package[$odoo::pip_package],
-          *        => $options,
-        }
+    package { $odoo_dependencies:
+      ensure => installed,
+    }
+
+    $odoo_pip_packages.each |$name, $options| {
+      package { $name:
+        provider => $odoo::pip_provider,
+        require  => Package[$odoo::pip_package],
+        *        => $options,
       }
     }
 
@@ -81,7 +76,10 @@ class odoo::dependencies {
       # https://www.odoo.com/documentation/13.0/setup/install.html
       # Odoo 10.0 depends on the python-xlwt package (old version), so install
       # the pip module on Odoo 11 and better.
-      unless fact('os.name') == 'debian' and versioncmp(fact('os.release.full'), '12.0') >= 0 {
+      unless (
+        (fact('os.name') == 'debian' and versioncmp(fact('os.release.full'), '12.0') >= 0) or
+        (fact('os.name') == 'ubuntu' and versioncmp(fact('os.release.full'), '24.04') >= 0)
+      ) {
         package { 'xlwt':
           ensure   => '1.3.0',
           provider => $odoo::pip_provider,
